c9efab750808efa5528bcd37f0fd1902a1ba6be1
Darius Petrov <dapetro@microsoft.com>
2019-09-25 15:00:23 +0300

    changed BugTypeDialog for Luis compatibility

---
+++ EchoBot1/Dialogs/BugTypeDialog.cs
@@ -0,0 +1,71 @@
+﻿using EchoBot1.Services;
+using Microsoft.Azure.CognitiveServices.Language.LUIS.Runtime.Models;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EchoBot1.Dialogs
+{
+    public class BugTypeDialog : ComponentDialog
+    {
+        #region Variables
+        private readonly BotStateService _botStateService;
+        private readonly BotServices _botServices;
+        #endregion
+
+        public BugTypeDialog(string dialogId, BotStateService botStateService, BotServices botServices) : base(dialogId)
+        {
+            _botStateService = botStateService ?? throw new System.ArgumentNullException(nameof(botStateService));
+            _botServices = botServices ?? throw new System.ArgumentNullException(nameof(botServices));
+
+            InitializeWaterfallDialog();
+        }
+
+        private void InitializeWaterfallDialog()
+        {
+            // Create Waterfall Steps
+            var waterfallSteps = new WaterfallStep[]
+            {
+                InitialStepAsync,
+                FinalStepAsync
+            };
+
+            // Add Named Dialog
+            AddDialog(new WaterfallDialog($"{nameof(BugTypeDialog)}.mainFlow", waterfallSteps));
+
+            // Set the starting dialog
+            InitialDialogId = $"{nameof(BugTypeDialog)}.mainFlow";
+        }
+
+        private async Task<DialogTurnResult> InitialStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            var result = await _botServices.Dispatch.RecognizeAsync(stepContext.Context, cancellationToken);
+            var luisResult = result.Properties["luisResult"] as LuisResult;
+            var entities = luisResult.Entities;
+
+            foreach (var entity in entities)
+            {
+                if (Conmmon.BugTypes.Any(s => s.Equals(entity.Entity, StringComparison.OrdinalIgnoreCase)))
+                {
+                    await stepContext.Context.SendActivityAsync(MessageFactory.Text(String.Format("Yes! {0} is a Bug Type!", entity.Entity)), cancellationToken);
+                }
+                else
+                {
+                    await stepContext.Context.SendActivityAsync(MessageFactory.Text(String.Format("No! {0} is not a Bug Type!", entity.Entity)), cancellationToken);
+                }
+            }
+
+            return await stepContext.NextAsync(null, cancellationToken);
+
+        }
+
+        private async Task<DialogTurnResult> FinalStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            return await stepContext.EndDialogAsync(null, cancellationToken);
+        }
+    }
+}
--- EchoBot1/Dialogs/MainDialog.cs
+++ EchoBot1/Dialogs/MainDialog.cs
@@ -1,4 +1,5 @@
 ﻿using EchoBot1.Services;
+using Microsoft.Bot.Builder;
 using Microsoft.Bot.Builder.Dialogs;
 using System;
 using System.Collections.Generic;
@@ -13,11 +14,13 @@ namespace EchoBot1.Dialogs
     {
         #region Variables
         private readonly BotStateService _botStateService;
+        private readonly BotServices _botServices;
         #endregion
 
-        public MainDialog(BotStateService botStateService) : base(nameof(MainDialog))
+        public MainDialog(BotStateService botStateService, BotServices botServices) : base(nameof(MainDialog))
         {
             _botStateService = botStateService ?? throw new System.ArgumentNullException(nameof(botStateService));
+            _botServices = botServices ?? throw new System.ArgumentNullException(nameof(botServices));
 
             InitializeWaterfallDialog();
         }
@@ -43,14 +46,26 @@ namespace EchoBot1.Dialogs
 
         private async Task<DialogTurnResult> InitialStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
         {
-            if (Regex.Match(stepContext.Context.Activity.Text.ToLower(), "hi").Success)
+            // First, we use the dispatch model to determine which cognitive service (LUIS or QnA) to use
+            var recognizerResult = await _botServices.Dispatch.RecognizeAsync(stepContext.Context, cancellationToken);
+
+            // To intent tell us which cognitive service to use
+            var topIntent = recognizerResult.GetTopScoringIntent();
+
+            switch (topIntent.intent)
             {
-                return await stepContext.BeginDialogAsync($"{nameof(MainDialog)}.greeting", null, cancellationToken);
+                case "Greeting":
+                    return await stepContext.BeginDialogAsync($"{nameof(MainDialog)}.greeting", null, cancellationToken);
+                case "NewBugReportIntent":
+                    return await stepContext.BeginDialogAsync($"{nameof(MainDialog)}.bugReport", null, cancellationToken);
+                case "QueryBugTypeIntent":
+                    return await stepContext.BeginDialogAsync($"{nameof(MainDialog)}.bugType", null, cancellationToken);
+                default:
+                    await stepContext.Context.SendActivityAsync(MessageFactory.Text($"I'm sorry, I don't know what you mean"), cancellationToken);
+                    break;
             }
-            else
-            {
-                return await stepContext.BeginDialogAsync($"{ nameof(MainDialog)}.bugReport", null, cancellationToken);
-            }
+
+            return await stepContext.NextAsync(null, cancellationToken);
         }
 
         private async Task<DialogTurnResult> FinalStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
